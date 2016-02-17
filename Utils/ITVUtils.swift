40119866b21f9cc6ce52c661c7b5bcd1939e0874
renanvs <renanvelososilva@hotmail.com>
2016-02-16 22:43:38 -0200

    download in cache

---
+++ Utils/ITVUtils.swift
@@ -0,0 +1,13 @@
+//
+//  ITVUtils.swift
+//  InstaTV
+//
+//  Created by renan silva on 2/16/16.
+//  Copyright © 2016 mwg. All rights reserved.
+//
+
+import UIKit
+
+class ITVUtils: NSObject {
+
+}
